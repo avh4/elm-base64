eae46cb0720796180401c7df3f339fc75faf708b
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-10-05 17:14:00 -0700

    Map integers to base64 characters

--- Base64.elm
+++ Base64.elm
@@ -1,7 +1,16 @@
 module Base64 (encode, decode) where
 
+import Array
+import List
+import String (..)
+
+chars = Array.fromList ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/']
+
+i2c : Int -> Char
+i2c i = Array.getOrFail i chars
+
 encode : String -> String
-encode s = s
+encode s = fromList <| List.map i2c [24, 22, 5, 33]
 
 decode : String -> Maybe String
 decode s = Just s
